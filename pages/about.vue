00f8ff9a90ae133a46d038cdb401bb95b2356d85
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-07-11 23:52:52 +0530

    About page with metatags

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is a app that displays corny dad jokes!</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return{
+            title:'About Us',
+            meta:[{
+                hid: 'description',
+                name: 'description',
+                content:'Best place for corny dad jokes'
+            }]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
